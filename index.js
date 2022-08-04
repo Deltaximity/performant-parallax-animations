0e514f259bf87f71dbed35fdeebf447d713c0107
Deltaximity <45205838+Deltaximity@users.noreply.github.com>
2022-08-04 20:39:17 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const fg = document.querySelector('#foreground');
+const s = document.querySelector('#subject');
+const bg = document.querySelector('#background');
+const sections = document.querySelectorAll('.sect');
+
+window.addEventListener('scroll', () => {
+    let offset = window.scrollY;
+    fg.style.top = "-" + offset*1.7 + "px";
+    s.style.top = "-" + offset*1.3 + "px";
+    bg.style.top = "-" + offset*1.1 + "px";
+});
+
+sections.forEach(section => {
+    let rand = Math.round(Math.random(5, 70) * 70);
+    console.log(rand);
+    section.style.padding = `0 0 0 ${rand}%`;
+});
